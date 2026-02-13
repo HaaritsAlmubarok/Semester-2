47744cc938105645b1f468713887667d7fa3f0ca
HaaritsAlmubarok <haaritsalmubarok8@gmail.com>
2026-02-13 21:25:41 +0700

    update project

--- Tugas/Tugas Koprasi Santri/main.py
+++ Tugas/Tugas Koprasi Santri/main.py
@@ -4,30 +4,38 @@ from barang import Barang
 from keranjang import Keranjang
 
 
+
 # ================= VALIDASI INPUT ANGKA =================
+
 def input_angka(pesan):
-    while True:
+    while True: # di ulang terus sampai inputnya bener
         try:
-            angka = int(input(pesan))
+            angka = int(input(pesan)) # kalo ketik huruf bakal error
             if angka < 0:
-                print("Angka tidak boleh negatif!")
+                print("\n Pilih 1 - 3 aja...")
             else:
                 return angka
         except ValueError:
-            print("Masukin angka woi!")
+            print("\n Masukin angka woi!") # jika error program akan menampilkan pesan ini (tidak crash)
+
 
 
 # ================= TAMPILKAN BARANG =================
+
 def tampilkan_barang(daftar_barang):
     print("\n=== DAFTAR MAKANAN ===")
-    for i, barang in enumerate(daftar_barang):
+    for i, barang in enumerate(daftar_barang): # enumerate untuk menampilkan index barang (1,2,3...) beserta isinya
         print(f"{i+1}. {barang.nama} | Harga: {barang.harga} | Stok: {barang.stok}")
+
+    # Menampilkan semua barang yang tersedia di toko beserta harga dan stoknya.
+
 
 
 # ================= MENU ADMIN =================
+
 def menu_admin(admin, daftar_barang):
-    while True:
-        print("\n=== MENU ADMIN ===")
+    while True: # program ngulang terus sampai admin pilih logout
+        print("\n   === MENU ADMIN ===\n")
         print("1. Tambah Makanan Baru")
         print("2. Tambah Stok")
         print("3. Lihat Daftar Makanan")
@@ -40,7 +48,7 @@ def menu_admin(admin, daftar_barang):
             harga = input_angka("Harga: ")
             stok = input_angka("Stok: ")
 
-            makanan_baru = Barang(nama, harga, stok)
+            makanan_baru = Barang(nama, harga, stok) # bikin object baru dari class Barang
             admin.tambah_barang_baru(daftar_barang, makanan_baru)
 
             print("Makanan berhasil ditambahkan!")
@@ -51,26 +59,28 @@ def menu_admin(admin, daftar_barang):
                 continue
 
             tampilkan_barang(daftar_barang)
-            index = input_angka("Pilih nomor makanan: ") - 1
+            index = input_angka("Pilih nomor makanan: ") - 1 # -1 karena index di list mulai dari 0
 
             if 0 <= index < len(daftar_barang):
                 jumlah = input_angka("Jumlah stok tambahan: ")
                 admin.tambah_stok_barang(daftar_barang[index], jumlah)
                 print("Stok berhasil ditambahkan!")
             else:
-                print("Nomor tidak valid!")
+                print("\n Isi yang bener...!")
 
         elif pilihan == "3":
             tampilkan_barang(daftar_barang)
 
         elif pilihan == "4":
-            break
+            break # menghentikan while True dan keluar dari menu admin
 
         else:
-            print("Pilihan tidak valid!")
+            print("\n Pilih yang ada di layar woi...!")
+
 
 
 # ================= MENU SANTRI =================
+
 def menu_santri(santri, daftar_barang):
     keranjang = Keranjang()
 
@@ -83,12 +93,14 @@ def menu_santri(santri, daftar_barang):
 
         pilihan = input("Pilih menu: ")
 
+        # ===== Lihat Daftar Makanan =====
         if pilihan == "1":
             tampilkan_barang(daftar_barang)
 
+        # ===== Tambah ke Keranjang =====
         elif pilihan == "2":
             if not daftar_barang:
-                print("Belum ada makanan!")
+                print("\n Belum ada makanan!")
                 continue
 
             tampilkan_barang(daftar_barang)
@@ -102,13 +114,14 @@ def menu_santri(santri, daftar_barang):
                 except ValueError as e:
                     print(e)
             else:
-                print("Nomor tidak valid!")
+                print("\n Belajar ngtik dulu...!")
 
+        # ===== Checkout =====
         elif pilihan == "3":
             total = keranjang.hitung_total()
 
             if total == 0:
-                print("Keranjang masih kosong!")
+                print("\n Keranjang masih kosong!")
                 continue
 
             print(f"Total belanja: {total}")
@@ -123,14 +136,17 @@ def menu_santri(santri, daftar_barang):
             except ValueError as e:
                 print(e)
 
+        # ===== Logout =====
         elif pilihan == "4":
             break
 
         else:
-            print("Pilihan tidak valid!")
+            print("\n Pilih yang bener...!")
+
 
 
 # ================= MAIN =================
+
 def main():
 
     daftar_barang = [
@@ -144,8 +160,8 @@ def main():
     santri = Santri("Santri", "Asrama Ibnu Katsir", "santri123", 50000)
 
 
-    while True:
-        print("\n      === SISTEM TOKO MAKANAN PONDOK ===\n")
+    while True: # program terus berjalan sampai user pilih keluar
+        print("\n\n      === SISTEM TOKO MAKANAN PONDOK ===\n")
         print("1. Login Admin")
         print("2. Login Santri")
         print("3. Keluar")
@@ -153,26 +169,30 @@ def main():
         pilihan = input("\nPilih menu: ")
 
         if pilihan == "1":
+            # verifikasi admin
             password = input("Masukkan password admin: ")
             if admin.cek_password(password):
                 menu_admin(admin, daftar_barang)
             else:
-                print("Password salah!")
+                print("\n Password salah!")
 
         elif pilihan == "2":
+            # verifikasi santri
             password = input("Masukkan password santri: ")
             if santri.cek_password(password):
                 menu_santri(santri, daftar_barang)
             else:
-                print("Password salah!")
+                print("\n Password salah!")
 
         elif pilihan == "3":
-            print("Program selesai.")
+            print("\n               === Program selesai ===")
+            print("   ================ Terima Kasih! ================")
             break
 
         else:
-            print("Pilihan tidak valid!")
+            print("\n Isi yang bener...!")
 
 
+# Jalankan fingsi main hanya jika file ini dijalankan langsung bukan diimpor sebagai modul
 if __name__ == "__main__":
     main()
